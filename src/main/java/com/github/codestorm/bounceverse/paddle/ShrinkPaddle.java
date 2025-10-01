f803b3c0c7acc6a262095e73beb96e0adcb05994
ManhTanTran <24021619@vnu.edu.vn>
2025-10-01 10:33:55 +0700

    Merge pull request #6
    * feat: Brick
    
    * feat(brick): add PowerBrick and update Brick, ExoplodeBrick, Protecte…
    
    * docs(brick): add Javadoc for Brick, ExoplodeBrick, PowerBrick, Protec…
    
    * refactor(brick): remove NormalBrick and StrongBrick
    
    * feat(paddle): add ExpendPaddle, LaserPaddle, ShrinkPaddle and update …
    
    * feat(paddle): add ExpendPaddle, LaserPaddle, ShrinkPaddle and update …
    
    * feat(paddle): add ExpendPaddle, LaserPaddle, ShrinkPaddle and update …
    
    * refactor: remove brick and gameManager packages
    
    * refactor(paddle): enhance documentation for Paddle and its variants

---
+++ src/main/java/com/github/codestorm/bounceverse/paddle/ShrinkPaddle.java
@@ -0,0 +1,18 @@
+package com.github.codestorm.bounceverse.paddle;
+
+/**
+ *
+ *
+ * <h1><b>ShrinkPaddle</b></h1>
+ *
+ * A special type of {@link Paddle} that is narrower than the normal paddle.
+ */
+public class ShrinkPaddle extends Paddle {
+    /** The scale factor applied to the paddle's width. */
+    private static final double SCALE = 0.7;
+
+    /** Create a new shrunk paddle. */
+    public ShrinkPaddle(int x, int y, int width, int height, double speed) {
+        super(x, y, (int) (width * SCALE), height, speed);
+    }
+}
