963e989234dfe03d99a54eea4af96496e2f027a1
Mai Thành <62001770+thnhmai06@users.noreply.github.com>
2025-10-09 17:43:36 +0700

    refactor: reorganize component structure and improve entity tagging (#12)
    * refactor: reorganize component structure and improve entity tagging
    
    - Added new classes for Ball, BrickDrop, BrickExplode, and BrickHealth to enhance gameplay mechanics (introducing new behaviors and properties).
    - Updated .gitignore to include additional files and directories for better project management.
    - Adjusted BrickFactory to streamline brick creation process.
    
    * refactor: enhance BrickFactory to include entity type for bricks
    
    Added EntityType.BRICK to the newBrick method for better entity classification. Updated documentation for OptionalTag to correct a typo.

---
+++ src/main/java/com/github/codestorm/bounceverse/components/properties/Health.java
@@ -0,0 +1,42 @@
+package com.github.codestorm.bounceverse.components.properties;
+
+import com.almasb.fxgl.dsl.components.HealthIntComponent;
+import com.almasb.fxgl.entity.component.Component;
+import com.github.codestorm.bounceverse.data.tags.components.PropertyComponent;
+
+/**
+ *
+ *
+ * <h1>{@link Health}</h1>
+ *
+ * <p>Lớp này đại diện cho thuộc tính HP của Entity. Khi HP về 0, Entity sẽ bị xóa khỏi thế giới.
+ */
+public abstract class Health extends Component implements PropertyComponent {
+    private final HealthIntComponent health;
+
+    public Health(int maxHealth) {
+        health = new HealthIntComponent(maxHealth);
+    }
+
+    public Health(HealthIntComponent health) {
+        this.health = health;
+    }
+
+    public HealthIntComponent getHealth() {
+        return health;
+    }
+
+    @Override
+    public void onAdded() {
+        super.onAdded();
+        entity.addComponent(health);
+    }
+
+    @Override
+    public void onUpdate(double tpf) {
+        super.onUpdate(tpf);
+        if (health.isZero()) {
+            entity.removeFromWorld();
+        }
+    }
+}
