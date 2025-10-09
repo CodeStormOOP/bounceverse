f11276aefaffe006916ac44a8d13618ca0fb946b
Mai Thành <62001770+thnhmai06@users.noreply.github.com>
2025-10-09 17:43:36 +0700

    refactor: reorganize component structure and improve entity tagging (#12)
    * refactor: reorganize component structure and improve entity tagging
    
    - Added new classes for Ball, BrickDrop, BrickExplode, and BrickHealth to enhance gameplay mechanics (introducing new behaviors and properties).
    - Updated .gitignore to include additional files and directories for better project management.
    - Adjusted BrickFactory to streamline brick creation process.
    
    * refactor: enhance BrickFactory to include entity type for bricks
    
    Added EntityType.BRICK to the newBrick method for better entity classification. Updated documentation for OptionalTag to correct a typo.

--- src/main/java/com/github/codestorm/bounceverse/Bounceverse.java
+++ src/main/java/com/github/codestorm/bounceverse/Bounceverse.java
@@ -6,6 +6,17 @@ import com.almasb.fxgl.dsl.FXGL;
 import com.github.codestorm.bounceverse.factory.BrickFactory;
 import javafx.scene.paint.Color;
 
+/**
+ *
+ *
+ * <h1>{@link Bounceverse}</h1>
+ *
+ * Phần Hệ thống Chương trình chính của game, nơi mà mọi thứ bắt đầu...
+ *
+ * <p><i>Game {@link Bounceverse} được lấy cảm hứng từ game Arkanoid nổi tiếng, nơi người chơi điều
+ * khiển một thanh để đỡ bóng và phá vỡ các viên gạch. Mục tiêu của game là phá vỡ tất cả các viên
+ * gạch và dành được điểm số cao nhất. Nhưng liệu mọi thứ chỉ đơn giản như vậy?</i>
+ */
 public final class Bounceverse extends GameApplication {
     public static final String name = "Bounceverse";
 
@@ -21,7 +32,7 @@ public final class Bounceverse extends GameApplication {
         FXGL.getGameWorld().addEntityFactory(new BrickFactory());
 
         var brick1 = FXGL.spawn("normalBrick", 100, 100);
-        var brick2 = FXGL.spawn("normalBrick", 100, 100);
+        var brick2 = FXGL.spawn("normalBrick", 200, 200);
     }
 
     @Override
