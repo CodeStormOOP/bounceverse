da286c0a40eb37a3369657c3f53047fc06f1528e
Mai Thành <62001770+thnhmai06@users.noreply.github.com>
2025-10-28 16:01:44 +0700

    [skip ci] chore: reformatting code

--- src/main/java/com/github/codestorm/bounceverse/scenes/PauseMenu.java
+++ src/main/java/com/github/codestorm/bounceverse/scenes/PauseMenu.java
@@ -2,12 +2,11 @@ package com.github.codestorm.bounceverse.scenes;
 
 import com.almasb.fxgl.app.scene.FXGLDefaultMenu;
 import com.almasb.fxgl.app.scene.MenuType;
-
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
-public class PauseMenu extends FXGLDefaultMenu{
+public class PauseMenu extends FXGLDefaultMenu {
     public PauseMenu() {
         super(MenuType.GAME_MENU);
 
