7550af6b9374c45b8fb7a9bc4f62fdc849c747a4
Trần Mạnh Tân <24021619@vnu.edu.vn>
2025-10-29 10:35:07 +0700

    chore: temp commit before switching device (update brick logic & add Special behavior)

--- src/main/java/com/github/codestorm/bounceverse/core/systems/GameSystem.java
+++ src/main/java/com/github/codestorm/bounceverse/core/systems/GameSystem.java
@@ -5,7 +5,9 @@ import com.almasb.fxgl.entity.EntityFactory;
 import com.github.codestorm.bounceverse.factory.entities.*;
 
 public final class GameSystem extends System {
-    private GameSystem() {}
+
+    private GameSystem() {
+    }
 
     public static GameSystem getInstance() {
         return GameSystem.Holder.INSTANCE;
@@ -37,9 +39,36 @@ public final class GameSystem extends System {
         spawnWalls();
 
         // Brick
-        for (int y = 1; y <= 6; y++) {
-            for (int x = 1; x <= 10; x++) {
-                FXGL.spawn("normalBrick", 85 * x, 35 * y);
+        int rows = 6;
+        int cols = 10;
+        double startX = 85;
+        double startY = 50;
+        double brickWidth = 80;
+        double brickHeight = 30;
+        double spacingX = 5;
+        double spacingY = 5;
+
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                double posX = startX + x * (brickWidth + spacingX);
+                double posY = startY + y * (brickHeight + spacingY);
+
+                // Xác định loại brick theo hàng (bạn có thể chỉnh lại tuỳ ý)
+                String type;
+                switch (y) {
+                    case 0 ->
+                        type = "shieldBrick";    // Hàng đầu có khiên
+                    case 1 ->
+                        type = "explodingBrick"; // Hàng thứ 2 nổ
+                    case 2 ->
+                        type = "specialBrick";   // Hàng thứ 3 rơi power-up
+                    case 3 ->
+                        type = "strongBrick";    // Hàng thứ 4 trâu
+                    default ->
+                        type = "normalBrick";   // Còn lại là thường
+                }
+
+                FXGL.spawn(type, posX, posY);
             }
         }
 
@@ -54,10 +83,12 @@ public final class GameSystem extends System {
 
     /**
      * Lazy-loaded singleton holder. <br>
-     * Follow <a href= "https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom">
+     * Follow
+     * <a href= "https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom">
      * Initialization-on-demand holder idiom</a>.
      */
     private static final class Holder {
+
         static final GameSystem INSTANCE = new GameSystem();
     }
 }
